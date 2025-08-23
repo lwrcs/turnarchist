295419b7957294db0ebfd7bfa3775132a9ea51d4
Alden Peffer <alden.peffer@gmail.com>
2025-08-23 13:19:10 -0400

    Update error handler

--- server/src/http/middleware/error-handler.ts
+++ server/src/http/middleware/error-handler.ts
@@ -2,7 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { logger } from "../../logger";
 import HttpStatus from "http-status-codes";
 
-export const errorHandler = (error: Error, _req: Request, res: Response) => {
+export const errorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
   logger.error(`Error: ${error.message}`);
 
   res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
