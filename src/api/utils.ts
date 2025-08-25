369375171579a6d8931ccb0a13353e69ce2f453e
Alden Peffer <alden.peffer@gmail.com>
2025-08-25 19:51:45 -0400

    Fixed URL

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -2,15 +2,15 @@
 // In development, it returns the local server URL.
 // In production, it returns the same domain as the current page.
 export const getEnvironmentApiUrl = () => {
-  const isDevelopment =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1" ||
-    window.location.hostname.includes("localhost") ||
-    window.location.protocol === "file:";
+  // const isDevelopment =
+  //   window.location.hostname === "localhost" ||
+  //   window.location.hostname === "127.0.0.1" ||
+  //   window.location.hostname.includes("localhost") ||
+  //   window.location.protocol === "file:";
 
-  if (isDevelopment) {
-    return "http://localhost:3000/api/v1";
-  } else {
-    return `https://turnarchist.onrender.com/api/v1/game/stats`;
-  }
+  // if (isDevelopment) {
+  //   return "http://localhost:3000/api/v1";
+  // } else {
+  return `https://turnarchist.onrender.com/api/v1`;
+  // }
 };
