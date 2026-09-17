716fb62646a201dfc197134ea3eb6110667b5ec1
lwrcs <harrisonkalman@gmail.com>
2026-09-17 04:28:59 -0400

    Deduplicate corrective navigation states

--- training/test_merge_navigation_data.py
+++ training/test_merge_navigation_data.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from merge_navigation_data import digest
+from merge_navigation_data import digest,unique_indices
 
 
 class MergeNavigationDataTests(unittest.TestCase):
@@ -11,6 +11,10 @@ class MergeNavigationDataTests(unittest.TestCase):
         other=np.array([1,2,4],dtype=np.float32)
         self.assertEqual(digest(first),digest(second))
         self.assertNotEqual(digest(first),digest(other))
+
+    def test_unique_indices_preserve_first_provenance_row(self):
+        observations=np.asarray([[1,2],[3,4],[1,2],[5,6],[3,4]],dtype=np.float32)
+        np.testing.assert_array_equal(unique_indices(observations),[0,1,3])
 
 
 if __name__=='__main__': unittest.main()
